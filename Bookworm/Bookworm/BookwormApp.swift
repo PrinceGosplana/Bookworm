a247fecac624bf46342a940e0418bca87af2993d
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-23 07:08:39 +0200

    init project

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by OLEKSANDR ISAIEV on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
