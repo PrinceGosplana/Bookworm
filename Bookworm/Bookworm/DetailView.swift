97b95995ed4a0d8ffda00cdceb247373fa75bf2b
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-25 08:13:28 +0200

    add detail view

---
+++ Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by OLEKSANDR ISAIEV on 25.12.2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
